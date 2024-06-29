1013157f73d0d585d848b1b693607b341b776919
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-06-29 10:18:47 +0800

    exercise 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Cart from './components/Cart';
-
-//Example e-commerce store. Navbar(totalShopping) and component card need to communicate. If the user adds an item, the number of total items in the navbar has to change.
-//Solution: use stateHook and lift it to the closest parent. App > Navbar, Cart
 
 function App() {
-  const [cartItems, setCartItems] = useState([
-    'Product1',
-    'Product2',
-    'Product3',
-  ]);
+  //Exercise 1: updating object with useState
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: 'John',
+      age: 30,
+    },
+  });
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: 'Tobias' } });
+  };
 
-  return (
-    <div>
-      <Navbar cartItemsCount={cartItems.length}></Navbar>
-      <Cart
-        cartItems={cartItems}
-        onClear={() => setCartItems([])}
-      ></Cart>
-    </div>
-  );
+  //Exercise 2
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom'],
+  });
+
+  const handleClick2 = () => {};
+
+  return <div></div>;
 }
 
 export default App;
