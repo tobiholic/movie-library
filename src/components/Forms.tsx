a8828b874c7e126cb81da5c93892771325deb77d
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-06-29 22:06:58 +0800

    forms 1.2

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { FormEvent, useRef } from 'react';
 
 const Forms = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: '',
+    age: 0,
+  };
+
+  //event handler for form
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = ageRef.current.valueAsNumber;
+    console.log(person);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='mb-3'>
         <label htmlFor='name' className='form-label'>
           Please enter your name
         </label>
-        <input id='name' type='text' className='form-control' />
+        <input ref={nameRef} id='name' type='text' className='form-control' />
       </div>
       <div className='mb-3'>
         <label htmlFor='' className='form-label'>
           Age
         </label>
-        <input type='number' className='form-control' />
+        <input ref={ageRef} type='number' className='form-control' />
       </div>
       <button type='submit' className='btn btn-primary'>
         send it
