acd2cc2627f49ed0712974167586bc72a7053eb4
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-06-30 17:24:00 +0800

    forms 1.3 managing forms

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormEvent, useRef } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 
 const Forms = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
-    name: '',
-    age: 0,
-  };
+  const { register, handleSubmit } = useForm();
+  console.log(register('name'));
 
-  //event handler for form
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = ageRef.current.valueAsNumber;
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
+  //Variante ONE
+
+  //define all inputs with null and safe it in variable
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
+  // const person = {
+  //   name: '',
+  //   age: 0,
+  // };
+
+  //event handler for form, ref hook
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if (nameRef.current !== null) person.name = nameRef.current.value;
+  //   if (ageRef.current !== null) person.age = ageRef.current.valueAsNumber;
+  //   console.log(person);
+  // };
+
+  //Attribute for input form
+  //ref={nameRef}
+  //Ref={ageRef}
+
+  //Variante TWO
+
+  //Managing form with reactHook
+  // const [person, setPerson] = useState({
+  //   name: '',
+  //   age: '',
+  // });
+
+  //Variant two, event handler onClick
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   console.log(person);
+  // };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={onSubmit}>
       <div className='mb-3'>
         <label htmlFor='name' className='form-label'>
           Please enter your name
         </label>
-        <input ref={nameRef} id='name' type='text' className='form-control' />
+        <input
+          //Variant TWO, using controlled components
+          // onChange={(event) =>
+          //   setPerson({ ...person, name: event.target.value })
+          // }
+          // value={person.name}
+          {...register('name')}
+          id='name'
+          type='text'
+          className='form-control'
+        />
       </div>
       <div className='mb-3'>
         <label htmlFor='' className='form-label'>
           Age
         </label>
-        <input ref={ageRef} type='number' className='form-control' />
+        <input
+          //Variant TWO, using controlled inputs
+          // onChange={(event) =>
+          //   setPerson({ ...person, age: parseInt(event.target.value) })
+          // }
+          // value={person.age}
+          {...register('age')}
+          type='number'
+          className='form-control'
+        />
       </div>
       <button type='submit' className='btn btn-primary'>
         send it
