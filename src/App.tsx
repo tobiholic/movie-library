62ef93a8b66aaaaab9eeaa5de8cfda52b15e99f5
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-07-02 17:48:01 +0800

    handling errors

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,22 @@ function App() {
   //setting user, defining <shape>
   const [user, setUser] = useState<User[]>([]);
 
+  const [error, setError] = useState('');
+
   //callback function Effect Hook to fetch data with axios
   useEffect(() => {
     const users = axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then((res) => setUser(res.data));
+      .get<User[]>('https://jsonplaceholder.typicode.com/xusers')
+      .then((res) => setUser(res.data))
+      .catch((err) => {
+        setError(err.message);
+      });
   }, []);
 
   return (
     <>
       <div>
+        {error && <p className="text-danger">{error}</p>}
         <ul>
           {user.map((user) => (
             <li key={user.id}>{user.name}</li>
