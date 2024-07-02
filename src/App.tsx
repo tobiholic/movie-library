953ac9d3e66d205b8b6156254009f3a9b05eda67
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-07-02 18:26:43 +0800

    final fetch incl. spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 
 //Defining attributes for users to show
 interface User {
@@ -10,23 +10,40 @@ interface User {
 function App() {
   //setting user, defining <shape>
   const [user, setUser] = useState<User[]>([]);
-
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   //callback function Effect Hook to fetch data with axios
   useEffect(() => {
-    const users = axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-      .then((res) => setUser(res.data))
+    const controller = new AbortController();
+
+    setLoading(true);
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUser(res.data), setLoading(false);
+      })
       .catch((err) => {
-        setError(err.message);
+        if (err instanceof CanceledError) return;
+        {
+          setError(err.message), setLoading(false);
+        }
       });
+    //not working with strict mode, using method 1, calling twice
+    // .finally(() => {
+    //   setLoading(false);
+    // });
+
+    return () => controller.abort();
   }, []);
 
   return (
     <>
       <div>
         {error && <p className="text-danger">{error}</p>}
+        {loading && <div className="spinner-border text-primary"></div>}
         <ul>
           {user.map((user) => (
             <li key={user.id}>{user.name}</li>
